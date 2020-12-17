94f468e9ca786b1a8750c792e4547b0e0ced7f6e
Ante Klincic <ante.klincic@gmail.com>
2020-12-17 04:22:00 +0100

    Dodan novi logo svg

--- navigation.php
+++ navigation.php
@@ -1,33 +1,34 @@
 <nav>
       <div class="logo">
-      <img src="/logo.png" alt="">
-      <a href="/" id="brand">MandarinaMoto<br>test</a></div>
-<a href="/" id="brand">ATV & SxS prodaja</a></div>
-<label for="btn" class="icon">
-        <span class="fa fa-bars"></span>
+      <a href="/"><img src="/logo.svg" class="logo2" alt=""></a>
+               
+            <!-- <a href="/" id="brand">MandarinaMoto <br>ATV & SxS prodaja</div> -->
+      </div>
+      <label for="btn" class="icon">
+            <span class="fa fa-bars"></span>
       </label>
       <input type="checkbox" id="btn">
       <ul>
-<li><a href="/">HOME</a></li>
-<li>
-          <label for="btn-1" class="show">ATV +</label>
-          <a href="#">ATV</a>
-          <input type="checkbox" id="btn-1">
-          <ul>
-<li><a href="/atv/cfmoto.php">CFMOTO</a></li>
-<li><a href="/atv/goes.php">GOES</a></li>
-</ul>
-</li>
-<li>
-          <label for="btn-2" class="show">SIDE-BY-SIDE +</label>
-          <a href="#">SIDE-BY-SIDE</a>
-          <input type="checkbox" id="btn-2">
-          <ul>
-<li><a href="/sxs/cfmoto.php">CFMOTO</a></li>
-<li><a href="/sxs/goes.php">GOES</a></li>
-</ul>
-</li>
-<li><a href="/motorcycles/">MOTORCYCLES</a></li>
-<li><a href="/#contactus">CONTACT US</a></li>
-</ul>
+            <li><a href="/">HOME</a></li>
+            <li>
+                  <label for="btn-1" class="show">ATV +</label>
+                  <a href="#">ATV</a>
+                  <input type="checkbox" id="btn-1">
+                  <ul>
+                        <li><a href="/atv/cfmoto.php">CFMOTO</a></li>
+                        <li><a href="/atv/goes.php">GOES</a></li>
+                  </ul>
+            </li>
+            <li>
+                  <label for="btn-2" class="show">SIDE-BY-SIDE +</label>
+                  <a href="#">SIDE-BY-SIDE</a>
+                  <input type="checkbox" id="btn-2">
+                  <ul>
+                        <li><a href="/sxs/cfmoto.php">CFMOTO</a></li>
+                        <li><a href="/sxs/goes.php">GOES</a></li>
+                  </ul>
+            </li>
+            <li><a href="/motorcycles/">MOTORCYCLES</a></li>
+            <li><a href="/#contactus">CONTACT US</a></li>
+      </ul>
 </nav>
